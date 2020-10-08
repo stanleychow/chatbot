6b4bd71d7ea6a029e4a549845132bdd44d784d11
stanleychow <chowtech1022@gmail.com>
2020-10-08 18:20:53 -0400

    Update chatbot.py
    fixed bugs

--- chatbot.py
+++ chatbot.py
@@ -5,18 +5,20 @@ def get_bot_response(user_response):
     bot_response_no = ["take a break!", "maybe next time!"]
     
     if user_response == "yes":
-        return choice(bot_reponse_yes)
+        return choice(bot_response_yes)
     elif user_response == "no":
         return choice(bot_response_no)
     else:
         return "See you later!"
+    
 
 print("Welcome to the Code Bot")
 print("Please enter whether or not you want to code")
 
 user_response = ""
+
 while True:
-    user_reponse = input("Do you want to code today?: ")
+    user_response = input("Do you want to code today?: ")
 
     if user_response == 'done':
         break
