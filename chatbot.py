ecef7d9f39edf61368dc25e05c441a6acb8d5c81
stanleychow <chowtech1022@gmail.com>
2020-10-08 18:04:06 -0400

    Update chatbot.py
    created if statements for get_bot_response

--- chatbot.py
+++ chatbot.py
@@ -4,4 +4,10 @@ def get_bot_response(user_response):
     bot_response_yes = ["happy coding!", "have fun!", "hello world!"]
     bot_response_no = ["take a break!", "maybe next time!"]
     
+    if user_response == "yes":
+        return choice(bot_reponse_yes)
+    elif user_response == "no":
+        return choice(bot_response_no)
+    else:
+        return "See you later!"
 
